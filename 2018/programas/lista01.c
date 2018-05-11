d5b1e2f986cb1029328edf21a13084e4f7b9bc4f
Prof. Julio Momente <prof.juliomomente@gmail.com>
2018-05-10 22:29:50 -0300

    conteúdos de aula

--- 2018/programas/lista01.c
+++ 2018/programas/lista01.c
@@ -7,14 +7,87 @@ typedef struct noLista {
 	struct noLista *prox;
 } Elemento;
 
+Elemento* inserirInicio(Elemento *Lista, Elemento *novo);
+Elemento* inserirFinal(Elemento *Lista, Elemento *novo);
+Elemento* inserirOrdem(Elemento *Lista, Elemento *novo);
+void imprimir(Elemento *Lista);
+
+
 main(){
 	Elemento *Lista = NULL, *novo;
+	int i;
 	
-	novo = (Elemento*) malloc(sizeof(Elemento));
-	printf("Informe um inteiro: ");
-	scanf("%d", &novo->info);
-	novo->prox = NULL;
+	for(i=0; i<6; i++) {
 	
-	Lista = novo;
+		novo = (Elemento*) malloc(sizeof(Elemento));
+		printf("Informe um inteiro: ");
+		scanf("%d", &novo->info);
+		novo->prox = NULL;
+	
+		Lista = inserirOrdem(Lista, novo);
+		imprimir(Lista);
+		printf("\n\n");
+	}
+	
+	
+	system("pause");
 	
 }
+
+
+Elemento* inserirInicio(Elemento *Lista, Elemento *novo){
+	Elemento *aux;
+	
+	aux = Lista;
+	novo->prox = aux;
+	Lista = novo;
+	return Lista;
+}
+
+Elemento* inserirFinal(Elemento *Lista, Elemento *novo){
+	Elemento *ultimo = Lista;
+	
+	if (Lista != NULL) {
+		while(ultimo->prox != NULL){
+			ultimo = ultimo->prox;
+		}
+		
+		ultimo->prox = novo;
+		novo->prox = NULL;
+	} else {
+		Lista = novo;
+	}
+	return Lista;
+}
+
+Elemento* inserirOrdem(Elemento *Lista, Elemento *novo){
+	Elemento *aux = Lista, *anterior;
+	
+	while (aux!=NULL && novo->info > aux->info){
+		aux=aux->prox;
+	}
+	if(aux == Lista) {
+		novo->prox = aux;
+		Lista = novo;
+	} else {
+		anterior = Lista;
+		while(anterior->prox != aux){
+			anterior = anterior->prox;
+		}
+		novo->prox = aux;
+		anterior->prox = novo;
+	}
+	return Lista;
+}
+
+
+void imprimir(Elemento *Lista){
+	Elemento *aux;
+	
+	aux = Lista;	
+	while (aux != NULL){
+		printf("%d\n", aux->info);
+		aux = aux->prox;
+	}
+}
+
