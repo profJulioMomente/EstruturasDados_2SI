e7aad8c2e7170d4f96d8fcf05fe9452c3e05348c
Prof. Julio Momente <prof.juliomomente@gmail.com>
2018-05-17 22:20:32 -0300

    conteúdos de aula

--- 2018/programas/lista01.c
+++ 2018/programas/lista01.c
@@ -10,12 +10,16 @@ typedef struct noLista {
 Elemento* inserirInicio(Elemento *Lista, Elemento *novo);
 Elemento* inserirFinal(Elemento *Lista, Elemento *novo);
 Elemento* inserirOrdem(Elemento *Lista, Elemento *novo);
+Elemento* pesquisar(Elemento *Lista, int valorBusca);
+Elemento* removerInicio(Elemento *Lista);
+Elemento* removerFinal(Elemento *Lista);
+Elemento* removerElemento(Elemento *Lista, int valorRemover);
 void imprimir(Elemento *Lista);
 
 
 main(){
-	Elemento *Lista = NULL, *novo;
-	int i;
+	Elemento *Lista = NULL, *novo, *resultadoPesquisa;
+	int i, valor;
 	
 	for(i=0; i<6; i++) {
 	
@@ -24,12 +28,29 @@ main(){
 		scanf("%d", &novo->info);
 		novo->prox = NULL;
 	
-		Lista = inserirOrdem(Lista, novo);
+		Lista = inserirInicio(Lista, novo);
 		imprimir(Lista);
 		printf("\n\n");
 	}
 	
-	
+	/*
+	printf("Informe um valor para pesquisa: ");
+	scanf("%d", &valor);
+	resultadoPesquisa = pesquisar(Lista, valor);
+	if(resultadoPesquisa != NULL) {
+		printf("Info = %d\n", resultadoPesquisa->info);
+	} else {
+		printf("Valor não encontrado\n");
+	}
+	*/
+	system("pause");
+	while(Lista != NULL){
+		printf("Informe um valor para remover: ");
+		scanf("%d", &valor);
+		Lista = removerElemento(Lista, valor);
+		imprimir(Lista);
+		system("pause");
+	}
 	system("pause");
 	
 }
@@ -89,5 +110,72 @@ void imprimir(Elemento *Lista){
 		printf("%d\n", aux->info);
 		aux = aux->prox;
 	}
+}
+
+
+Elemento* pesquisar(Elemento *Lista, int valorBusca){
+	Elemento *aux;
+	
+	aux = Lista;
+	while(aux!=NULL && aux->info != valorBusca){
+		aux = aux->prox;
+	}
+	return aux;
+}
+
+Elemento* removerInicio(Elemento *Lista){
+	Elemento *remover;
+	
+	if (Lista!=NULL){
+		remover = Lista;
+		Lista = Lista->prox;
+		
+		free(remover);
+	}
+	return Lista;
+}
+
+Elemento* removerFinal(Elemento *Lista){
+	Elemento *remover, *anterior;
+	
+	if (Lista != NULL){
+		remover = Lista;
+		while(remover->prox != NULL) {
+			remover = remover->prox;
+		}
+		if(remover != Lista){
+			anterior = Lista;
+			while(anterior->prox != remover){
+				anterior = anterior->prox;
+			}
+			anterior->prox = NULL;
+		} else {
+			Lista = NULL;
+		}
+		free(remover);
+	}
+	return Lista;
+}
+
+
+Elemento* removerElemento(Elemento *Lista, int valorRemover){
+	Elemento *remover, *anterior;
+	
+	remover = pesquisar(Lista, valorRemover);
+	if(remover !=NULL){
+		if(remover == Lista){
+			Lista = Lista->prox;
+		} else{
+			anterior = Lista;
+			while(anterior->prox != remover){
+				anterior = anterior->prox;
+			}
+			anterior->prox = remover->prox;
+		}
+		free(remover);
+	} else {
+		printf("Elemento nao encontrado\n");
+	}
+	return Lista;
 }
 
